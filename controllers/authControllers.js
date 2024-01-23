345678be52cb8c344523e61ae4e37f500c484ba1
shobhit-joshi-8 <shobhitjoshi20259@acropolis.in>
2024-01-23 22:06:52 +0530

    added login functionality

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -43,4 +43,40 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = { registerController };
+// LOGIN
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide Email Or Password",
+      });
+    }
+
+    // check user
+    const user = await userModel.findOne({ email: email, password: password });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User Not Found Or Password Mismatch",
+      });
+    }
+    res.send({
+      success: true,
+      message: "Login Succesfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Login API",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, loginController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { registerController } = require("../controllers/authControllers");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/authControllers");
 
 const router = express.Router();
 
 // routes
 // REGISTER || POST
 router.post("/register", registerController);
+
+// LOGIN
+router.post("/login", loginController);
 
 module.exports = router;
